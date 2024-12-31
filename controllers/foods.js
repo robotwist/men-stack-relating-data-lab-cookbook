6ac49bf615a048e6b50dedf5903a5ad2287bb6c6
Rob Wistrand <robwistrand@gmail.com>
2024-12-31 15:50:43 -0800

    delete rt  refactored trycatch

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,63 +3,50 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 // Index - GET /users/:userId/foods
-router.get('/users/:userId/foods', async (req, res) => {
-  const user = await User.findById(req.params.userId);
-  res.json(user.pantry);
-});
-
-// Index - GET /users/:userId/foods
-router.get('/', (req, res) => {
-  res.render('foods/index.ejs');
-});
-
-// New - GET /users/:userId/foods/new
-router.get('/users/:userId/foods/new', (req, res) => {
-  res.send('Form to create a new food item');
+router.get('/', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId).populate('pantry');
+    res.render('foods/index.ejs', { pantry: user.pantry, userId: req.params.userId });
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
 });
 
 // New - GET /users/:userId/foods/new
-router.get('/new', (req, res) => {
-  res.render('foods/new.ejs', { userId: req.params.userId });
+router.get('/new', async (req, res) => {
+  try {
+    res.render('foods/new.ejs', { userId: req.params.userId });
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
 });
 
 // Create - POST /users/:userId/foods
-router.post('/users/:userId/foods', async (req, res) => {
-  const user = await User.findById(req.params.userId);
-  user.pantry.push(req.body);
-  await user.save();
-  res.json(user.pantry);
-});
-
-// Show - GET /users/:userId/foods/:itemId
-router.get('/users/:userId/foods/:itemId', async (req, res) => {
-  const user = await User.findById(req.params.userId);
-  const foodItem = user.pantry.id(req.params.itemId);
-  res.json(foodItem);
-});
-
-// Edit - GET /users/:userId/foods/:itemId/edit
-router.get('/users/:userId/foods/:itemId/edit', async (req, res) => {
-  const user = await User.findById(req.params.userId);
-  const foodItem = user.pantry.id(req.params.itemId);
-  res.send(`Form to edit food item: ${foodItem.name}`);
-});
-
-// Update - PUT /users/:userId/foods/:itemId
-router.put('/users/:userId/foods/:itemId', async (req, res) => {
-  const user = await User.findById(req.params.userId);
-  const foodItem = user.pantry.id(req.params.itemId);
-  foodItem.set(req.body);
-  await user.save();
-  res.json(foodItem);
+router.post('/', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    user.pantry.push(req.body);
+    await user.save();
+    res.redirect(`/users/${req.params.userId}/foods`);
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
 });
 
 // Delete - DELETE /users/:userId/foods/:itemId
-router.delete('/users/:userId/foods/:itemId', async (req, res) => {
-  const user = await User.findById(req.params.userId);
-  user.pantry.id(req.params.itemId).remove();
-  await user.save();
-  res.sendStatus(204);
+router.delete('/:itemId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    user.pantry.id(req.params.itemId).remove();
+    await user.save();
+    res.redirect(`/users/${req.params.userId}/foods`);
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
