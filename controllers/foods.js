753863a75124e530ee57556f0753b085cb6e9bb5
Rob Wistrand <robwistrand@gmail.com>
2024-12-31 17:59:48 -0800

    edit and update functionality

--- controllers/foods.js
+++ controllers/foods.js
@@ -6,6 +6,10 @@ const User = require('../models/user.js');
 router.get('/', async (req, res) => {
   try {
     const user = await User.findById(req.params.userId).populate('pantry');
+    if (!user) {
+      console.error('User not found');
+      return res.redirect('/');
+    }
     res.render('foods/index.ejs', { pantry: user.pantry, userId: req.params.userId });
   } catch (error) {
     console.error(error);
@@ -27,6 +31,10 @@ router.get('/new', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const user = await User.findById(req.params.userId);
+    if (!user) {
+      console.error('User not found');
+      return res.redirect('/');
+    }
     user.pantry.push(req.body);
     await user.save();
     res.redirect(`/users/${req.params.userId}/foods`);
@@ -36,10 +44,47 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Edit - GET /users/:userId/foods/:itemId/edit
+router.get('/:itemId/edit', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      console.error('User not found');
+      return res.redirect('/');
+    }
+    const foodItem = user.pantry.id(req.params.itemId);
+    res.render('foods/edit.ejs', { userId: req.params.userId, foodItem });
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
+});
+
+// Update - PUT /users/:userId/foods/:itemId
+router.put('/:itemId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      console.error('User not found');
+      return res.redirect('/');
+    }
+    const foodItem = user.pantry.id(req.params.itemId);
+    foodItem.set(req.body);
+    await user.save();
+    res.redirect(`/users/${req.params.userId}/foods`);
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
+});
 // Delete - DELETE /users/:userId/foods/:itemId
 router.delete('/:itemId', async (req, res) => {
   try {
     const user = await User.findById(req.params.userId);
+    if (!user) {
+      console.error('User not found');
+      return res.redirect('/');
+    }
     user.pantry.id(req.params.itemId).remove();
     await user.save();
     res.redirect(`/users/${req.params.userId}/foods`);
